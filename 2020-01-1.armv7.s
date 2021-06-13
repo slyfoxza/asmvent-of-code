69e2f6db0c29fcbfdc4761ec886688191e696956
Philip Cronje <syke107@gmail.com>
2021-06-13 22:54:22 +0200

    armv7 for 2020-01 part 2
    The armv7 solution for 2020-01 part 1 is also tweaked to use different
    registers, and actually preserve the caller registers it should be per
    the calling convention.

--- 2020-01-1.armv7.s
+++ 2020-01-1.armv7.s
@@ -2,59 +2,63 @@
 .text
 .global main
 main:
-	# Save the link register to the stack so that we know where to return to
-	# later, given we'll also be calling printf which would clobber the LR.
-	push {lr}
+	# Adhere to the calling convention: we're going to use R4 and R5, so we
+	# need to preserve their values from the caller. On top of that, we're
+	# going to call out to (f)printf, so we need to keep track of the link
+	# register so we don't lose track of where we should return to.
+	push {r4-r5, lr}
 
-	# Set R9 to the constant value 2020. We'll use it as the operand to a CMP
+	# Set R5 to the constant value 2020. We'll use it as the operand to a CMP
 	# later, since 2020 is too large to use as an immediate value in the CMP
 	# instruction.
-	mov r9, $2020
+	mov r5, $2020
 
 	# Since the very first instruction after the loop_a_start label is going to
-	# increment R4 by 1, initialise R4 to all bits 1, so that the increment
+	# increment R0 by 1, initialise R0 to all bits 1, so that the increment
 	# will overflow to zero, starting the first iteration as desired.
-	mvn r4, $0
+	mvn r0, $0
 
 .Lloop_a_start:
-	add r4, $1
-	cmp r4, max_iter_a
+	add r0, $1
+	cmp r0, max_iter_a
 	beq .Lloop_a_completed
 
-	# Load the input value at index R4 into R6. We'll keep it there for the
+	# Load the input value at index R0 into R2. We'll keep it there for the
 	# remainder of the iteration to avoid repeatedly fetching it from RAM.
 	#
-	# It first loads the address of input into R6 using an ADR instruction, and
+	# It first loads the address of input into R2 using an ADR instruction, and
 	# then uses LDR to calculate the appropriate offset using a 2-bit left-
-	# shifted R4; in other words: R6 = input + R4*4
-	adr r6, input
-	ldr r6, [r6, r4, LSL#2]
+	# shifted R0; in other words: R2 = input + R0*4
+	adr r2, input
+	ldr r2, [r2, r0, LSL#2]
 
-	mov r5, r4
+	mov r1, r0
 .Lloop_b_start:
-	add r5, $1
-	cmp r5, max_iter_b
+	add r1, $1
+	cmp r1, max_iter_b
 	beq .Lloop_a_start
 
-	# As above, load the next input value into R7. While we'll only use it once
+	# As above, load the next input value into R3. While we'll only use it once
 	# in this iteration, if it's the value that makes the sum match the target,
 	# we can again re-use it when calculating the product that will be the
 	# algorithm result.
-	adr r7, input
-	ldr r7, [r7, r5, LSL#2]
+	adr r3, input
+	ldr r3, [r3, r1, LSL#2]
 
 	# Test the sum against the target value, and repeat the inner loop if it's
 	# no match.
-	add r8, r6, r7
-	cmp r8, r9
+	add r4, r2, r3
+	cmp r4, r5
 	bne .Lloop_b_start
 
-	# If we didn't branch, then we've found the pair we're looking for.
+	# If we didn't branch, then we've found the pair we're looking for. Since
+	# we're out of the loop, we can now freely clobber R0 and R1 which
+	# previously held the loop indices.
 	ldr r0, =format_string
-	mul r1, r6, r7
+	mul r1, r2, r3
 	bl printf  @ printf(format_string, product)
 
-	mov r0, #0; pop {pc}  @ return 0
+	mov r0, #0; pop {r4-r5, pc}  @ return 0
 
 .Lloop_a_completed:
 	ldr r0, =stderr
@@ -62,7 +66,7 @@ main:
 	ldr r1, =error_string
 	bl fprintf  @ fprintf(stderr, error_string)
 
-	mov r0, #1; pop {pc}  @ return 1
+	mov r0, #1; pop {r4-r5, pc}  @ return 1
 
 .include "2020-01.txt.s"
 .set max_iter_b, (. - input) / 4
---
+++ 2020-01-2.armv7.s
@@ -0,0 +1,64 @@
+.syntax unified
+.text
+# This part is going to have less comments, since it's basically the same as in
+# 2020-01-1, except for the addition of another nested loop.
+.global main
+main:
+	# Thanks to the extra loop, we use 2 more registers that need their values
+	# preserved for the caller.
+	push {r4-r7, lr}
+	mov r7, $2020
+	mvn r0, $0
+
+.Lloop_a_start:
+	add r0, $1
+	cmp r0, max_iter_a
+	beq .Lloop_a_completed
+
+	adr r3, input
+	ldr r3, [r3, r0, LSL#2]
+
+	mov r1, r0
+.Lloop_b_start:
+	add r1, $1
+	cmp r1, max_iter_b
+	beq .Lloop_a_start
+
+	adr r4, input
+	ldr r4, [r4, r1, LSL#2]
+
+	mov r2, r1
+.Lloop_c_start:
+	add r2, $1
+	cmp r2, max_iter_c
+	beq .Lloop_b_start
+
+	adr r5, input
+	ldr r5, [r5, r2, LSL#2]
+
+	add r6, r3, r4
+	add r6, r6, r5
+	cmp r6, r7
+	bne .Lloop_c_start
+
+	ldr r0, =format_string
+	mul r1, r3, r4
+	mul r1, r5, r1
+	bl printf  @ printf(format_string, product)
+
+	mov r0, $0; pop {r4-r7, pc}  @ return 0
+
+.Lloop_a_completed:
+	ldr r0, =stderr
+	ldr r0, [r0]
+	ldr r1, =error_string
+	bl fprintf  @ fprintf(stderr, error_string)
+
+	mov r0, $1; pop {r4-r7, pc}  @ return 1
+
+.include "2020-01.txt.s"
+.set max_iter_c, (. - input) / 4
+.set max_iter_b, max_iter_c - 1
+.set max_iter_a, max_iter_b - 1
+format_string: .asciz "%d\n"
+error_string: .asciz "Error: no result found!\n"
